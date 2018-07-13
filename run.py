d591428310b8482ced54f4f10c4e645b1a373ec1
Severus <pengshichenno.1@live.com>
2018-07-13 15:37:53 +0800

    renew main function.

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from problems import DTLZ1
 from algorithms import NSGAII
-
+import numpy as np
 if __name__ == "__main__":
-    problem = DTLZ1.DTLZ1()
-    population, boundary, coding = problem.init(generation_size=10)
+    problem = DTLZ1.DTLZ1(M=5)
+    population, boundary, coding = problem.init(generation_size=3)
     func_value = problem.value(population)
-    NSGAII.fast_non_dominated_sort(func_value)
+    print('func_value = \n{0}'.format(func_value))
+    Front,rank = NSGAII.fast_non_dominated_sort(func_value)
+    print('Rank = \n{0}'.format(rank))
