65e14a92bea1e27796d9ff19585f2d00d44c3f0e
anonymone <pengshichenno.1@live.com>
2018-07-14 23:01:43 +0800

    add cross over function (exit bug)

--- public/tools.py
+++ public/tools.py
@@ -50,5 +50,19 @@ def mating_pool(population, popu_rank, crow_distance):
                 pool[i,:] = population[select2,:]
     return pool
 
-def cross_over():
-    pass
+def cross_over(parent,proC,disC,proM,disM):
+    N,M = parent.shape
+    parent1 = parent[0:int(N/2),:]
+    parent2 = parent[int(N/2)+1:,:]
+    beta = np.zeros(N/2,M)
+    mu = np.random.rand(N/2,M)
+    mu_index = np.where(mu <= 0.5)
+    beta[mu_index] = (2*mu(mu_index)**(1/(disC+1)))
+    mu_index = np.where(mu > 0.5)
+    beta[mu_index] = (2-2*mu[mu_index])**(-1/(disC+1))
+    beta = beta*(-1)**np.random.rand(N/2,M)
+    mu_index = np.where(np.random.rand(N/2,M)<0.5)
+    beta[mu_index] = 1
+    beta[np.where(np.tile(np.random.rand(N/2,1)>proC,[1,M]))] = 1
+    offspring = np.tile((parent1 + parent2)/2+beta*(parent1-parent2)/2,[2,1])
+    return offspring
