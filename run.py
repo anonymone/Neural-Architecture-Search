0119c8e2a3e0cc42cbc94cb2de0b89f7937a564c
Severus <pengshichenno.1@live.com>
2018-07-15 11:23:00 +0800

    renew main function

--- run.py
+++ run.py
@@ -2,28 +2,31 @@ from problems import DTLZ1
 from algorithms import NSGAII
 from public import tools
 import numpy as np
+
 if __name__ == "__main__":
     problem = DTLZ1.DTLZ1(M=3)
-    population, boundary, coding = problem.init(generation_size=100)
+    population, boundary, coding = problem.init(generation_size=10)
     print('population = \n{0}'.format(population))
     func_value = problem.value(population)
-
-    # func_value = [ [39.45757807, 109.22284033], 
-    #                 [2.86965248,  91.65383822],
-    #                 [229.9125652,   33.88312065],
-    #                 [175.35145278, 196.53583365],
-    #                 [143.62980809, 123.4551886],  
-    #                 [68.24446104, 203.41141973],
-    #                 [363.35745327,  36.28574311],
-    #                 [56.44501436, 253.76417207], 
-    #                 [187.71003021,  34.78490607],
-    #                 [155.58596971, 56.68474627]]
-    # func_value = np.array(func_value)
-
     print('func_value = \n{0}'.format(func_value))
-    Front,rank = NSGAII.fast_non_dominated_sort(func_value)
+    Front, rank = NSGAII.fast_non_dominated_sort(func_value)
     print('Rank = \n{0}'.format(rank))
     crowd_distance = NSGAII.crowding_distance(func_value)
     print('crowd_distance = \n{0}'.format(crowd_distance))
-    mating_p = tools.mating_pool(population,rank,crowd_distance)
+    mating_p = tools.mating_pool(population, rank, crowd_distance)
     print('mating_p = \n{0}'.format(mating_p))
+    offspring = tools.real_cross_over(parent=mating_p)
+    offspring = tools.real_mutation(offspring, boundary)
+    print("offspring = \n{0}".format(offspring))
+
+    # func_value = [ [39.45757807, 109.22284033],
+    #                 [2.86965248,  91.65383822],
+    #                 [229.9125652,   33.88312065],
+    #                 [175.35145278, 196.53583365],
+    #                 [143.62980809, 123.4551886],
+    #                 [68.24446104, 203.41141973],
+    #                 [363.35745327,  36.28574311],
+    #                 [56.44501436, 253.76417207],
+    #                 [187.71003021,  34.78490607],
+    #                 [155.58596971, 56.68474627]]
+    # func_value = np.array(func_value)
